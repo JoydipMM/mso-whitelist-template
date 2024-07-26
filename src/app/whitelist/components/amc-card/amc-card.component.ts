a6ace46e14cf013f3aed852ec6cc5127ae3bc755
joydip <joydip@matrixnmedia.com>
2024-07-26 17:10:25 +0530

    unpublised and published change

--- src/app/whitelist/components/amc-card/amc-card.component.ts
+++ src/app/whitelist/components/amc-card/amc-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-amc-card',
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AmcCardComponent implements OnInit {
 
+  @Input() amc:any;
+  @Output() publishEventEmitter = new EventEmitter();
+  @Output() unpublishEventEmitter = new EventEmitter();
+
   constructor() { }
 
   ngOnInit(): void {
+
+    //console.log(this.amc)
+  }
+
+  publishAction(id:number){
+    console.log("published ", id);
+    this.publishEventEmitter.emit({...this.amc, published: true, id: id});
+  }
+
+  unpublishAction(id:number){
+    console.log("Unpublished ", id);
+    this.unpublishEventEmitter.emit({...this.amc, published: false, id: id});
   }
 
 }
--- src/app/whitelist/pages/home/home.component.ts
+++ src/app/whitelist/pages/home/home.component.ts
@@ -10,10 +10,78 @@ export class HomeComponent implements OnInit {
   publishedPerPage = 10;
   createdPerPage = 10;
 
+  demoamclist = [
+    {
+      id: 1,
+      title: 'Aditya Birla Sun Life Mutual Fund',
+      filename: 'Commissioning List Mar,2024',
+      date: '21st Feb. 2024',
+      published: false,
+      created: false,
+    },
+    {
+      id: 2,
+      title: 'Aditya Birla Sun Life Mutual Fund 2',
+      filename: 'Commissioning List Apr,2024',
+      date: '21st Feb. 2024',
+      published: false,
+      created: true,
+    },
+    {
+      id: 3,
+      title: 'Aditya Birla Sun Life Mutual Fund 3',
+      filename: 'Commissioning List May,2024',
+      date: '22nd Feb. 2024',
+      published: false,
+      created: true,
+    },
+    {
+      id: 4,
+      title: 'Aditya Birla Sun Life Mutual Fund 4',
+      filename: 'Commissioning List June,2024',
+      date: '23rd Feb. 2024',
+      published: false,
+      created: true,
+    },
+    {
+      id: 5,
+      title: 'Aditya Birla Sun Life Mutual Fund 5',
+      filename: 'Commissioning List July,2024',
+      date: '24th Feb. 2024',
+      published: true,
+      created: true,
+    },
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
     
+  }
+
+  displayUpdated(event:any){
+    console.log("updated info ", event);
+    var id = event.id;
+    //var publshStatus = event.published;
+    //console.log("id---- ", id, 'published---- ', publshStatus);
+
+    let indexToUpdate = this.demoamclist.findIndex(item => item.id === id);
+    this.demoamclist[indexToUpdate] = event;
+    console.log(this.demoamclist[indexToUpdate]);
+
+    this.demoamclist = Object.assign([], this.demoamclist);
+  }
+
+  falseCheck(data:any){
+    let is_publish;
+    data.forEach((element:any) => {
+      if(element.published == true){
+        is_publish = true;
+      }
+    });
+    //console.log(is_publish);
+    return is_publish;
+
   }
 
 }
