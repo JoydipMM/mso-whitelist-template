574c0ce7234187a68b14732c2ad51a8d15c3b3f9
joydip <joydip@matrixnmedia.com>
2024-08-05 19:04:44 +0530

    brokerage drap and drop table UI work in progress

--- src/app/whitelist/pages/brokerage-upload/brokerage-upload.component.ts
+++ src/app/whitelist/pages/brokerage-upload/brokerage-upload.component.ts
@@ -35,11 +35,60 @@ export class BrokerageUploadComponent implements OnInit {
 
   dragActive:boolean = false;
 
+  /* amc name */
+  amcnameList:any = [];
+  selectedamcnameItems:any = [];
+  amcdropdownSettings:any = {};
+
   constructor() {
 
    }
 
   ngOnInit(): void {
+     /* amc name */
+     this.amcnameList = [
+      { item_id: 1, item_text: 'ICICI Prudential Infrastructure Fund' },
+      { item_id: 2, item_text: 'ICICI Prudential BHARAT 22 FOF Scheme' },
+      { item_id: 3, item_text: 'Kotak Infrastructure and Economic Reform Fund' },
+      { item_id: 4, item_text: 'HSBC Small Cap Fund' },
+      { item_id: 5, item_text: 'Aditya Birla Sun Life Infrastructure Fund' }
+    ];
+    this.selectedamcnameItems = [
+      { item_id: 3, item_text: 'Kotak Infrastructure and Economic Reform Fund' },
+      { item_id: 4, item_text: 'HSBC Small Cap Fund' }
+    ];
+    this.amcdropdownSettings = {
+      singleSelection: true,
+      allowSearchFilter: true,
+      enableCheckAll: false,
+      idField: 'item_id',
+      textField: 'item_text',
+      selectAllText: 'Select All',
+      unSelectAllText: 'UnSelect All',
+      itemsShowLimit: 1,
+    };
+  }
+
+
+  public onFilterChange(item: any) {
+    console.log(item);
+  }
+  public onDropDownClose(item: any) {
+    console.log(item);
+  }
+
+  public onItemSelect(item: any) {
+    console.log(item);
+  }
+  public onDeSelect(item: any) {
+    console.log(item);
+  }
+
+  public onSelectAll(items: any) {
+    console.log(items);
+  }
+  public onDeSelectAll(items: any) {
+    console.log(items);
   }
 
 
@@ -83,6 +132,11 @@ export class BrokerageUploadComponent implements OnInit {
       this.displayedColumns.pop();
     }
   }
+  removeColumnDynamic(i:any) {
+    if (this.displayedColumns) {
+      this.displayedColumns.pop();
+    }
+  }
 
   getColumnIndex(name: string): number {
     return this.displayedColumns.indexOf(name);
@@ -120,7 +174,6 @@ export class BrokerageUploadComponent implements OnInit {
     this.dataSource.pop();
     this.table.renderRows();
   }
-
 
 
 
